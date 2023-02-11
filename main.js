45190b5ddcd28c4152590e01b3aac9f6c7fba828
Mustafa-isa <116859259+Mustafa-isa@users.noreply.github.com>
2023-02-11 02:10:51 +0200

    start slider image "simple"
    start coding

---
+++ main.js
@@ -0,0 +1,26 @@
+//start code right now hahahahah
+let images = Array.from(document.querySelectorAll("img"))
+let slideNumber =document.getElementById('slide-number')
+let prev =document.getElementById("prev")
+let next =document.getElementById("next")
+let indicators =document.getElementById("indicators")
+//add functions on click
+
+
+
+
+prev.addEventListener("click" ,prevFuncion)
+next.addEventListener("click" ,nextFuncion)
+
+//add functions right now
+
+function prevFuncion(){
+    console.log("hi mustafa from console")
+
+
+}
+
+function nextFuncion(){
+
+    console.log("hi mustafa from console")
+}
